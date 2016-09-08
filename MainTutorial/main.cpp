3cdf34c49f71b5c7d386af375aaddd5f823dd9ab
Owner <Owner@DESKTOP-DLMP2BP>
2016-09-08 00:46:48 -0700

    Connected SDL window and OpenGL context hopefully, added websites as a comment to remind self

--- MainTutorial/main.cpp
+++ MainTutorial/main.cpp
@@ -5,11 +5,15 @@
 
 const int SCREEN_HEIGHT = 800;
 const int SCREEN_WIDTH = 1200;
+const int OPEN_GL_VERSION = 2;  // OpenGL version 3.x
+const int USE_DOUBLE_BUFFER = 1;  //1 = use double buffering
 
+
+//See :  lazyfoo.net for SDL2 stuff, learnopengl.com for OpenGL stuff, headerphile.com/sdl/ for combining the two
 
 int main(int argc, char* argv[])
 {
-	return 0;
+	//return 0;
 
 	SDL_Window* window;
 	SDL_Surface* surface;
@@ -17,6 +21,7 @@ int main(int argc, char* argv[])
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		return -1;
 	}
 
 	else
@@ -26,6 +31,25 @@ int main(int argc, char* argv[])
 		if (window == NULL)
 		{
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			return -1;
+		}
+
+		else
+		{
+			/*
+			int SDL_GL_SetAttribute
+			(
+				SDL_GLattr attr,
+				int value
+			)
+			*/
+
+			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, OPEN_GL_VERSION);  //OpenGL ver 3.2
+
+			SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, USE_DOUBLE_BUFFER);  //Self-explanatory
+
+			SDL_GLContext SDL_GL_CreateContext(window);
+			//glewExperimental = true;   //Uncomment to use later OpenGL stuff
 		}
 	}
 }
