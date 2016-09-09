f54533cf3ba3e5a8278e9cb8f93804fab1ede736
MidnightDrifter <linkmasterchief@aol.com>
2016-09-08 19:13:22 -0700

    zzz

--- MainTutorial/main.cpp
+++ MainTutorial/main.cpp
@@ -5,7 +5,8 @@
 
 const int SCREEN_HEIGHT = 800;
 const int SCREEN_WIDTH = 1200;
-const int OPEN_GL_VERSION = 2;  // OpenGL version 3.x
+const int OPEN_GL_MAJOR_VERSION = 3; //OpenGL version 3.x
+const int OPEN_GL_MINOR_VERSION = 2;  // OpenGL version x.2
 const int USE_DOUBLE_BUFFER = 1;  //1 = use double buffering
 //Woo
 int test;
@@ -17,6 +18,8 @@ int main(int argc, char* argv[])
 
 	SDL_Window* window;
 	SDL_Surface* surface;
+	SDL_GLContext context;
+
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
@@ -43,13 +46,34 @@ int main(int argc, char* argv[])
 				int value
 			)
 			*/
-
-			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, OPEN_GL_VERSION);  //OpenGL ver 3.2
+			SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, OPEN_GL_MAJOR_VERSION);
+			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, OPEN_GL_MINOR_VERSION);  //OpenGL ver 3.2
 
 			SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, USE_DOUBLE_BUFFER);  //Self-explanatory
 
-			SDL_GLContext SDL_GL_CreateContext(window);
+			context= SDL_GL_CreateContext(window);
 			//glewExperimental = true;   //Uncomment to use later OpenGL stuff
+
+			glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			SDL_GL_SwapWindow(window);
+
+			//Fill window with color--in this case, black
+
+
+			//GAME LOOP HERE
+
+			//Destroy context and window
+
+			
+			SDL_GL_DeleteContext(context);
+
+			// Destroy our window
+			SDL_DestroyWindow(window);
+
+			// Shutdown SDL 2
+			SDL_Quit();
 		}
 	}
 }
